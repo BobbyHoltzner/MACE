d0840e9eeb8debea4236afaff1b1cdc47b02cad8
MadisonBlake <Madison.Blake@heronsystems.com>
2016-08-24 14:27:25 -0400

    Forgot main

---
+++ src/mace/mace.pro
@@ -0,0 +1,36 @@
+TEMPLATE = app
+CONFIG += console c++11
+CONFIG -= app_bundle
+CONFIG -= qt
+
+SOURCES += main.cpp \
+    data_interpolation.cpp
+
+QMAKE_CXXFLAGS += -std=c++11
+
+win32:CONFIG(release, debug|release): LIBS += -L$$OUT_PWD/../mace_core/release/ -lmace_core
+else:win32:CONFIG(debug, debug|release): LIBS += -L$$OUT_PWD/../mace_core/debug/ -lmace_core
+else:unix: LIBS += -L$$OUT_PWD/../mace_core/ -lmace_core
+
+
+win32:CONFIG(release, debug|release): LIBS += -L$$OUT_PWD/../module_path_planning_NASAPhase2/release/ -lmodule_path_planning_NASAPhase2
+else:win32:CONFIG(debug, debug|release): LIBS += -L$$OUT_PWD/../module_path_planning_NASAPhase2/debug/ -lmodule_path_planning_NASAPhase2
+else:unix: LIBS += -L$$OUT_PWD/../module_path_planning_NASAPhase2/ -lmodule_path_planning_NASAPhase2
+
+
+win32:CONFIG(release, debug|release): LIBS += -L$$OUT_PWD/../module_vehicle_MAVLINK/release/ -lmodule_vehicle_MAVLINK
+else:win32:CONFIG(debug, debug|release): LIBS += -L$$OUT_PWD/../module_vehicle_MAVLINK/debug/ -lmodule_vehicle_MAVLINK
+else:unix: LIBS += -L$$OUT_PWD/../module_vehicle_MAVLINK/ -lmodule_vehicle_MAVLINK
+
+
+win32:CONFIG(release, debug|release): LIBS += -L$$OUT_PWD/../module_RTA_NASAPhase2/release/ -lmodule_RTA_NASAPhase2
+else:win32:CONFIG(debug, debug|release): LIBS += -L$$OUT_PWD/../module_RTA_NASAPhase2/debug/ -lmodule_RTA_NASAPhase2
+else:unix: LIBS += -L$$OUT_PWD/../module_RTA_NASAPhase2/ -lmodule_RTA_NASAPhase2
+
+
+
+INCLUDEPATH += $$PWD/../
+DEPENDPATH += $$PWD/../
+
+HEADERS += \
+    data_interpolation.h
---
+++ src/mace/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <thread>
+
+#include "mace_core/mace_core.h"
+
+#include "module_RTA_NASAPhase2/module_rta_nasaphase2.h"
+#include "module_vehicle_MAVLINK/module_vehicle_mavlink.h"
+#include "module_path_planning_NASAPhase2/module_path_planning_nasaphase2.h"
+
+
+#include "data_interpolation.h"
+
+
+int main(int argc, char *argv[])
+{
+
+    //Initialize core and configure data object
+    MaceCore::MaceCore core;
+    std::shared_ptr<MaceCore::MaceData> data = std::make_shared<DataInterpolation>();
+    core.AddDataFusion(data);
+
+
+    // Add RTA Module
+    MaceCore::Metadata_RTA RTAMetaData;
+    std::shared_ptr<ModuleRTANASAPhase2> rtaModule = std::make_shared<ModuleRTANASAPhase2>(RTAMetaData);
+    rtaModule->setDataObject(data);
+    core.AddRTAModule(rtaModule);
+
+
+    //Add Path Planning Module
+    MaceCore::MetadataPathPlanning pathPlanningData;
+    std::shared_ptr<ModulePathPlanningNASAPhase2> pathPlanningModule = std::make_shared<ModulePathPlanningNASAPhase2>(pathPlanningData);
+    pathPlanningModule->setDataObject(data);
+    core.AddPathPlanningModule(pathPlanningModule);
+
+
+    //Add Vehicle(s)
+    MaceCore::MetadataVehicle vehicle1MetaData;
+    std::shared_ptr<ModuleVehicleMAVLINK> vehicle1Module = std::make_shared<ModuleVehicleMAVLINK>(vehicle1MetaData);
+    vehicle1Module->setDataObject(data);
+    core.AddVehicle("Vehicle1", vehicle1Module);
+
+
+    //Start Threads
+    std::thread rta_thread ([&](){rtaModule->start();});
+    std::thread pathPlanning_thread ([&](){pathPlanningModule->start();});
+    std::thread vehicle1_thread ([&](){vehicle1Module->start();});
+
+
+    //Wait for thread to finish
+    rta_thread.join();
+    pathPlanning_thread.join();
+    vehicle1_thread.join();
+
+    return 0;
+}
