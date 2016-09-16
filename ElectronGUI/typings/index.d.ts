2aef8491942a3ca4a6c658f28ae5610130e2d44b
patnolan33 <pat.nolan@heronsystems.com>
2016-09-16 19:35:14 -0400

    Revert "Initial Commit of supporting code."
    This reverts commit 3a222526332407874046db668da448d661c63a2c.

--- ElectronGUI/typings/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="globals/leaflet/index.d.ts" />
-/// <reference path="globals/material-ui/index.d.ts" />
-/// <reference path="globals/react-dom/index.d.ts" />
-/// <reference path="globals/react/index.d.ts" />
