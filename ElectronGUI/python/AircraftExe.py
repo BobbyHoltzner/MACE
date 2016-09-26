907a136e7005d4e0c9d7e9281f5c70d1b6bb99f4
majorpr13 <kkroeger@araustech.com>
2016-09-26 15:25:03 -0400

    Updated Functioning Vehicle Exe
    There was a primary issue with the vehicle exe code prior to the demo in
    that when making frequent requests to the API for the vehicle state, the
    API had not been thread safe in the request and ultimately returned a
    value that would break the cycle of the aircraft performing the guided
    mission. This however is corrected by creating a callback and handling
    the udpate inside there. The next commit shall remove the guided
    activation per mode change (This was done so testing could take place
    without requiring a GUI to fire the aircraft state)

--- ElectronGUI/python/AircraftExe.py
+++ ElectronGUI/python/AircraftExe.py
@@ -1,41 +1,85 @@
+#!/usr/bin/env python
 import sys, struct, time, os, threading, math
+import dronekit_sitl
+import logging
 from pymavlink.dialects.v20 import common as mavlink
-from dronekit import connect, VehicleMode, LocationGlobal, LocationGlobalRelative
+from dronekit import connect, VehicleMode, Command, LocationGlobal, LocationGlobalRelative
 from pymavlink import mavutil
 from argparse import ArgumentParser
 from threading import Thread
 
+LOG_FILENAME = 'AircraftTesting.log'
+logging.basicConfig(filename=LOG_FILENAME,level=logging.DEBUG)
+
+logging.debug("This is the begginning")
+
 missionLength = 0
 currentMissionIndex = 0
-latArray = []
-lngArray = []
-altArray = []
 
-def vehicle_LocationCallback(self, attr_name, value):
-    print("The vehicle location is %f %f %f: " %(value.lat, value.lon, value.alt))
+latArray = [35.738618,35.73825,35.738251,35.738618,35.738618,35.738253,35.738254,35.738618,35.738618,35.738255,35.738256,35.738618]
+lngArray = [-78.847804,-78.847814,-78.847739,-78.847729,-78.847654,-78.847664,-78.847589,-78.847579,-78.847504,-78.847514,-78.847439,-78.847429]
+altArray = [5.0,5.0,5.0,5.0,5.0,5.0,5.0,5.0,5.0,5.0,5.0,5.0]
 
+stopThread = True
+last_mode_cache = "STABILIZE"
+
+def performMissionFunciton():
+    cmds = vehicle.commands
+    cmds.clear()
+    for i in range(0,len(latArray),1):
+        cmd1 = Command(0,0,0,mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0, 0, latArray[i], lngArray[i], altArray[i])
+        cmds.add(cmd1)
+    cmds.upload()                 
+    vehicle.airspeed = 3
+
+    for i in range(0, len(latArray), 1):
+        if stopThread == True:
+            break
+        else:
+            goto(latArray[i], lngArray[i], altArray[i], 3)
+
+missionThread = Thread(target=performMissionFunciton)
+
+def vehicleModeCallback(self, attr_name, value):
+    # `attr_name` - the observed attribute (used if callback is used for multiple attributes)
+    # `value` is the updated attribute value.
+    global last_mode_cache
+    global stopThread
+    global missionThread
+    # Only publish when value changes
+    if value!=last_mode_cache:
+        print " CALLBACK: Mode changed to", value
+        last_mode_cache=value
+        if(value == "GUIDED"):
+            print "The mode was now in guided"
+            stopThread = False
+            missionThread = Thread(target=performMissionFunciton)
+            missionThread.start()
+        else:
+            if missionThread.isAlive():
+                stopThread = True
+                missionThread.join()
+                print "The mission thread was stopped"
+            print "We have changed into something other than guided"
 
 def get_distance_metres(aLocation1, aLocation2):
     dlat = aLocation2.lat - aLocation1.lat
     dlong = aLocation2.lon - aLocation1.lon
     return math.sqrt((dlat * dlat) + (dlong * dlong)) * 1.113195e5
 
-
 def goto(latitude, longitude, altitude, radius):
+    global stopThread
     print "I am in the go to function"
     targetLocation = LocationGlobal(latitude, longitude, altitude)
     goto_position_target_global_int(targetLocation)
 
-    while vehicle.mode.name == "GUIDED":  # Stop action if we are no longer in guided mode.
-        # print "DEBUG: mode: %s" % vehicle.mode.name
+    while stopThread == False:  # Stop action if we are no longer in guided mode.
         remainingDistance = get_distance_metres(vehicle.location.global_relative_frame, targetLocation)
-        print "Distance to target: ", remainingDistance
         #goto_position_target_global_int(targetLocation)
         if remainingDistance <= radius:  # Just below target, in case of undershoot.
             print "Reached target"
             break;
-        time.sleep(1)
-
+        #time.sleep(1)
 
 def goto_position_target_global_int(targetLocation):
     msg = vehicle.message_factory.set_position_target_global_int_encode(
@@ -55,7 +99,6 @@ def goto_position_target_global_int(targetLocation):
     # send command to vehicle
     vehicle.send_mavlink(msg)
 
-
 def arm_and_takeoff(targetAltitude):
     while not vehicle.is_armable:
         print("The vehicle cannot arm in this state")
@@ -69,18 +112,11 @@ def arm_and_takeoff(targetAltitude):
 
     print "I have told the vehicle to takeoff to an altitude of %s" %targetAltitude
     vehicle.simple_takeoff(targetAltitude)
-#Even though it was convienent to know when the vehicle was done climbing...it was impractical as it blocked any
-#other events from happening...and launching on own thread doesnt seem practical
-#    while True:
-#        if vehicle.location.global_relative_frame.alt >= targetAltitude * 0.95:
-#            break
-#        time.sleep(0.5)
-#    print "The vehicle is done climbing"
 
 parser = ArgumentParser(description=__doc__)
 parser.add_argument("--baudrate", type=int,
                   help="master port baud rate", default=57600)
-parser.add_argument("--device", help="serial device", default="com5")
+parser.add_argument("--device", help="serial device", default="/dev/ttyUSB0")
 parser.add_argument("--rate", default=4, type=int, help="requested stream rate")
 parser.add_argument("--source-system", dest='SOURCE_SYSTEM', type=int,
                   default=255, help='MAVLink source system for this GCS')
@@ -95,20 +131,17 @@ def heartbeatTimer(m, interval):
         mavlinkMSG.heartbeat_send(mavlink.MAV_TYPE_GCS, mavlink.MAV_AUTOPILOT_GENERIC, 0, 0, mavlink.MAV_STATE_ACTIVE)
 
         home = LocationGlobal(vehicle.home_location.lat, vehicle.home_location.lon, vehicle.home_location.alt)
-        mavlinkMSG.mission_item_send(0, 0, 0, 0, 0, 0, 0, vehicle.location.global_frame.lat,
-                                     vehicle.location.global_frame.lon, vehicle.location.global_frame.alt, vehicle.heading, home.lat,
-                                     home.lon, home.alt)
-
-        #msg = m.recv_match(type="COMMAND_ACK", blocking=False)
-        #if msg:
-        #    break
-
+        mavlinkMSG.mission_item_send(0, 0, 0, 0, 0, 0, 0, vehicle.location.global_relative_frame.lat, vehicle.location.global_relative_frame.lon,vehicle.location.global_relative_frame.alt, vehicle.heading, vehicle.home_location.lat, vehicle.home_location.lon, vehicle.home_location.alt)
 
 def handleMissionMessage(m):
     mavlinkMSG = mavlink.MAVLink(m)
+    global vehicle
+    global currentMissionIndex
     global latArray
     global lngArray
     global altArray
+    global missionThread
+    global stopThread
 
     '''show incoming mavlink messages'''
     while True:
@@ -119,37 +152,52 @@ def handleMissionMessage(m):
         msgID = msg.get_msgId()
         if msgID == mavlink.MAVLINK_MSG_ID_MISSION_ITEM:
             if(msg.command == mavlink.MAV_CMD_NAV_WAYPOINT):
-                print"I saw a new mission object for index %s" % msg.seq
                 index = msg.seq + 1
+                logging.debug("I saw a new mission item at index %s",index)
                 latArray.insert(index, msg.x)
                 lngArray.insert(index, msg.y)
                 altArray.insert(index, msg.z)
                 currentMissionIndex = currentMissionIndex + 1
                 if currentMissionIndex < missionLength:
+                    logging.debug("Making a request for the item at %s",currentMissionIndex)
                     mavlinkMSG.mission_request_send(m.target_system, m.target_component, currentMissionIndex)
                 else:
-                    print "I am done with requesting mission items"
+                    logging.debug("I am done requesting mission items")
+                    # This will clear the current commands inside the vehicle for auto mode
+                    cmds = vehicle.commands
+                    cmds.clear()
+                    for i in range(0,len(latArray),1):
+                        mavlink.MAVLink_command_long_message
+                        cmd1 = Command(0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0, 0, latArray[i], lngArray[i], altArray[i])
+                        cmds.add(cmd1)
+                    cmds.upload()
             elif(msg.command == mavlink.MAV_CMD_NAV_TAKEOFF):
-                print "I am going to perform a takeoff"
                 arm_and_takeoff(20)
             elif(msg.command == mavlink.MAV_CMD_NAV_RETURN_TO_LAUNCH):
-                print "I am going to perform a RTL"
                 vehicle.mode = VehicleMode("RTL")
             elif(msg.command == mavlink.MAV_CMD_NAV_LAND):
-                print "I am going to perform a land"
                 vehicle.mode = VehicleMode("LAND")
             elif(msg.command == mavlink.MAV_CMD_DO_GUIDED_MASTER):
                 print "I have been allowed to perform my guided mode"
-                thread = Thread(target = performMissionFunciton)
-                thread.start()
+                if missionThread.isAlive():
+                    print "I am already in the mission mode"
+                else:
+                    vehicle.mode = VehicleMode("GUIDED")
+                    stopThread = False
+                    missionThread = Thread(target=performMissionFunciton)
+                    missionThread.start()
         elif msgID == mavlink.MAVLINK_MSG_ID_MISSION_CLEAR_ALL:
-            print "I saw a clear all command"
+            logging.debug("I saw a clear all message")
+            vehicle.mode = VehicleMode("LOITER")
+            if missionThread.isAlive():
+                stopThread = True
+                missionThread.join()
             latArray = []
             lngArray = []
             altArray = []
             #probably want to halt motion and wait for further commands as well
         elif msgID == mavlink.MAVLINK_MSG_ID_MISSION_COUNT:
-            print("I saw a mission count %s" %msg.count)
+            logging.debug("I saw a mission count message")
             missionLength = msg.count
             currentMissionIndex = 0
             mavlinkMSG.mission_request_send(m.target_system, m.target_component, currentMissionIndex)
@@ -160,35 +208,26 @@ def handleMissionMessage(m):
         else:
             print(msg)
 
-
-def performMissionFunciton():
-    for i in range(0, len(latArray), 1):
-        goto(latArray[i], lngArray[i], altArray[i], 5)
-
+time.sleep(10)
 if __name__ == '__main__':
-    vehicle = connect("udp:127.0.0.1:14551", wait_ready=True)
-# Get Vehicle Home location - will be `None` until first set by autopilot
+    #vehicle = connect("udp:0.0.0.0:14550", wait_ready=False)
+    vehicle = connect("/dev/ttySAC0", baud=57600, wait_ready=False)
+    vehicle.add_attribute_listener('mode', vehicleModeCallback)
+
+    cmds = vehicle.commands
+    cmds.clear()
+    # Get Vehicle Home location - will be `None` until first set by autopilot
     while not vehicle.home_location:
-        cmds = vehicle.commands
-        cmds.download()
-        cmds.wait_ready()
         if not vehicle.home_location:
             print " Waiting for home location ..."
-
+            time.sleep(1)
     # We have a home location, so print it!
     print "\n Home location: %s" % vehicle.home_location
 
-    # create a mavlink serial instance that connects to the ground station
-    # mavutil.mavlink_connection("com5", 57600)
     master = mavutil.mavlink_connection(args.device, baud=args.baudrate)
 
     mavlinkMSG = mavlink.MAVLink(master)
     heartbeatThread = Thread(target = heartbeatTimer, args = (master,1))
     heartbeatThread.start()
-    #heartbeatTimer(master, 2)
-#    home = LocationGlobal(vehicle.home_location.lat, vehicle.home_location.lon, vehicle.home_location.alt)
-#    mavlinkMSG.mission_item_send(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, home.lat, home.lon, home.alt)
 
     handleMissionMessage(master)
-
-    #handleFlightMessages(master)
