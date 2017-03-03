651951fc52115ee1cbbb897a0cda20e6f19f2081
kkroeger007 <ken.kroeger@heronsystems.com>
2017-03-03 09:46:18 -0500

    Commit chanes

--- src/data_vehicle_ardupilot/mace_to_ardupilot.h
+++ src/data_vehicle_ardupilot/mace_to_ardupilot.h
@@ -5,6 +5,9 @@
 
 #include "mavlink.h"
 
+#include "data/coordinate_frame.h"
+#include "data/positional_coordinate_frame.h"
+
 #include "data_generic_state_item/state_item_components.h"
 #include "data_generic_state_item_topic/state_topic_components.h"
 
@@ -20,7 +23,7 @@ namespace DataArdupilot{
     mavlink_message_t generateSetHomePosition(const MissionItem::SpatialHome &vehicleHome, const int &chan);
 
     mavlink_message_t generateTakeoffMessage(const MissionItem::SpatialTakeoff<DataState::StateGlobalPosition> missionItem, const uint8_t &chan, const uint8_t &compID);
-    mavlink_message_t generateArdupilotCommandMessage(std::shared_ptr<MissionItem::AbstractMissionItem> missionItem, const uint8_t &chan, const uint8_t &compID, const uint16_t &itemIndex);
+    mavlink_message_t generateArdupilotCommandMessage(std::shared_ptr<MissionItem::AbstractMissionItem> missionItem, const uint8_t &chan, const uint8_t &compID);
     mavlink_message_t MACEMissionToMAVLINKMission(std::shared_ptr<MissionItem::AbstractMissionItem> missionItem, const uint8_t &chan, const uint8_t &compID, const uint16_t &itemIndex);
 
     mavlink_message_t MACEGuidedToArdupilot(std::shared_ptr<MissionItem::AbstractMissionItem> missionItem, const uint8_t &chan, const uint8_t &compID);
