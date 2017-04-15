37441a956355f94ec3e6c6e75a191c45a12577bc
majorpr13 <kkroeger@araustech.com>
2017-04-15 08:38:29 -0400

    Fix Comms Issues

--- mavlink_cpp/Stable/mace/testsuite.h
+++ mavlink_cpp/Stable/mace/testsuite.h
@@ -12,22 +12,16 @@ extern "C" {
 
 #ifndef MAVLINK_TEST_ALL
 #define MAVLINK_TEST_ALL
-static void mavlink_test_common(uint8_t, uint8_t, mavlink_message_t *last_msg);
-static void mavlink_test_uAvionix(uint8_t, uint8_t, mavlink_message_t *last_msg);
 static void mavlink_test_ardupilotmega(uint8_t, uint8_t, mavlink_message_t *last_msg);
 static void mavlink_test_MACE(uint8_t, uint8_t, mavlink_message_t *last_msg);
 
 static void mavlink_test_all(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
 {
-    mavlink_test_common(system_id, component_id, last_msg);
-    mavlink_test_uAvionix(system_id, component_id, last_msg);
     mavlink_test_ardupilotmega(system_id, component_id, last_msg);
     mavlink_test_MACE(system_id, component_id, last_msg);
 }
 #endif
 
-#include "../common/testsuite.h"
-#include "../uAvionix/testsuite.h"
 #include "../ardupilotmega/testsuite.h"
 
 
@@ -664,6 +658,64 @@ static void mavlink_test_mace_mission_ack(uint8_t system_id, uint8_t component_i
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
 }
 
+static void mavlink_test_mace_roi_ag(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
+{
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+    mavlink_status_t *status = mavlink_get_channel_status(MAVLINK_COMM_0);
+        if ((status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) && MAVLINK_MSG_ID_MACE_ROI_AG >= 256) {
+            return;
+        }
+#endif
+    mavlink_message_t msg;
+        uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
+        uint16_t i;
+    mavlink_mace_roi_ag_t packet_in = {
+        17.0,17,84,151,218
+    };
+    mavlink_mace_roi_ag_t packet1, packet2;
+        memset(&packet1, 0, sizeof(packet1));
+        packet1.stress_value = packet_in.stress_value;
+        packet1.target_system = packet_in.target_system;
+        packet1.target_component = packet_in.target_component;
+        packet1.point_discovery = packet_in.point_discovery;
+        packet1.stress_threshold = packet_in.stress_threshold;
+        
+        
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+        if (status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) {
+           // cope with extensions
+           memset(MAVLINK_MSG_ID_MACE_ROI_AG_MIN_LEN + (char *)&packet1, 0, sizeof(packet1)-MAVLINK_MSG_ID_MACE_ROI_AG_MIN_LEN);
+        }
+#endif
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_mace_roi_ag_encode(system_id, component_id, &msg, &packet1);
+    mavlink_msg_mace_roi_ag_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_mace_roi_ag_pack(system_id, component_id, &msg , packet1.target_system , packet1.target_component , packet1.point_discovery , packet1.stress_threshold , packet1.stress_value );
+    mavlink_msg_mace_roi_ag_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_mace_roi_ag_pack_chan(system_id, component_id, MAVLINK_COMM_0, &msg , packet1.target_system , packet1.target_component , packet1.point_discovery , packet1.stress_threshold , packet1.stress_value );
+    mavlink_msg_mace_roi_ag_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+        mavlink_msg_to_send_buffer(buffer, &msg);
+        for (i=0; i<mavlink_msg_get_send_buffer_length(&msg); i++) {
+            comm_send_ch(MAVLINK_COMM_0, buffer[i]);
+        }
+    mavlink_msg_mace_roi_ag_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+        
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_mace_roi_ag_send(MAVLINK_COMM_1 , packet1.target_system , packet1.target_component , packet1.point_discovery , packet1.stress_threshold , packet1.stress_value );
+    mavlink_msg_mace_roi_ag_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+}
+
 static void mavlink_test_MACE(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
 {
     mavlink_test_mace_mission_request_partial_list(system_id, component_id, last_msg);
@@ -677,6 +729,7 @@ static void mavlink_test_MACE(uint8_t system_id, uint8_t component_id, mavlink_m
     mavlink_test_mace_mission_clear_all(system_id, component_id, last_msg);
     mavlink_test_mace_mission_item_reached(system_id, component_id, last_msg);
     mavlink_test_mace_mission_ack(system_id, component_id, last_msg);
+    mavlink_test_mace_roi_ag(system_id, component_id, last_msg);
 }
 
 #ifdef __cplusplus
