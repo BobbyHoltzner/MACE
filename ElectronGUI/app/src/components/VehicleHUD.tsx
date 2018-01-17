6343e4390c365e755c7663f3ea2c72e8e262600a
patnolan33 <pat.nolan@heronsystems.com>
2018-01-17 10:20:01 -0500

    Fix vehicle mode changes from HUD
    Vehicle mode change was always sending UNKNOWN.

--- ElectronGUI/app/src/components/VehicleHUD.tsx
+++ ElectronGUI/app/src/components/VehicleHUD.tsx
@@ -128,7 +128,7 @@ export class VehicleHUD extends React.Component<Props, State> {
         for(let index in this.props.aircraft.availableModes) {
             let mode = this.props.aircraft.availableModes[index];
             modeMenuItems.push(
-                <MenuItem key={mode} onClick={() => this.handleModeChange(this.props.aircraft.availableModes[index])} style={{color: 'rgba(0,0,0,0.7)', paddingLeft: 0, paddingRight: 0, paddingTop: 0, paddingBottom: 0, marginLeft: 0, marginRight: 0}} value={mode} primaryText={mode} label={mode} />
+                <MenuItem key={mode} onClick={() => this.handleModeChange(mode)} style={{color: 'rgba(0,0,0,0.7)', paddingLeft: 0, paddingRight: 0, paddingTop: 0, paddingBottom: 0, marginLeft: 0, marginRight: 0}} value={mode} primaryText={mode} label={mode} />
             )
         }
 
